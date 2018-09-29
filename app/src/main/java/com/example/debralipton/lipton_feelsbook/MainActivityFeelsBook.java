8d7e22e3beff904c0260bda79c581ceaff407711
Debra Lipton <>
2018-09-29 12:29:24 -0600

    first commit

---
+++ app/src/main/java/com/example/debralipton/lipton_feelsbook/MainActivityFeelsBook.java
@@ -0,0 +1,13 @@
+package com.example.debralipton.lipton_feelsbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityFeelsBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_feels_book);
+    }
+}
